54fe3f875343332bf00f5b3851980540fd26d36c
Pranjali Bishnoi <pranjali.bishnoi@gmail.com>
2024-05-25 02:46:14 +0530

    Delete CRS-JEDI-FLIPKART-JAVA-POS/src directory

--- CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/business/RegisteredCourseInterface.java
+++
@@ -1,6 +0,0 @@
-package com.flipkart.business;
-
-public interface RegisteredCourseInterface {
-	public void dropCourseByAdmin();
-	public void addCourseByAdmin();
-}
--- CRS-JEDI-FLIPKART-JAVA-POS/src/com/flipkart/dao/RegisteredCourseDAOInterface.java
+++
@@ -1,7 +0,0 @@
-package com.flipkart.dao;
-
-public interface RegisteredCourseDAOInterface {
-	public void addCourseByAdmin();
-	public void dropCourseByAdmin();
-
-}
